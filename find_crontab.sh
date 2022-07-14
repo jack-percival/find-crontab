07ccd9d4d6c1240358a0e03160bc08a1664600a6
root <root@DESKTOP-70QGD1I.localdomain>
2022-07-14 13:30:06 +0100

    Initial commit of find_crontab.sh

--- find_crontab.sh
+++ find_crontab.sh
@@ -1,19 +1,52 @@
+#!/bin/bash -x
 # if statement to check if the folder exists and if it doesn't to create it
-if [ ! -d /home/jack/documents/scripts/old_pathways ]; then
+if [ ! -d /home/jack/documents/scripts/old_cleanup_paths ]; then
 	# make directory if it does not exist
-	mkdir -p /home/jack/documents/scripts/old_pathways;
+	mkdir -p /home/jack/documents/scripts/old_cleanup_paths;
 else
 echo "folder already exists"
 
-# copies out previous day's pathways file and renames it with the date at the end
-cp /home/jack/documents/scripts/pathways /home/jack/documents/scripts/pathways_"$(date)"
+if [ $? -eq 0 ]; then
+	echo "folder created or already exists"
+else
+	echo "mkdir failed"
+	exit
+fi
+
+# copies out previous day's pathways file and renames it with the date at the end.
+cp /home/jack/documents/scripts/cleanup_paths /home/jack/documents/scripts/old_cleanup_paths/cleanup_paths_"$(date)"
+
+if [ $? -eq 0 ]; then
+	        echo "copied succesfully"
+	else
+		echo "copy failed"
+		        exit
+fi
 
 # retrieves new pathways file from url
-wget -p /home/jack/documents/scripts https://github.com/jack-percival/Find-crontab/blob/main/pathways.sh
+# wget -O is used because we only want to download that specific file not the whole html code
+wget -O /home/jack/documents/scripts/cleanup_paths https://raw.githubusercontent.com/jack-percival/Find-crontab/main/cleanup_paths
 
-file="/home/jack/documents/scripts/pathways"
-lines=$(cat $file)
+if [ $? -eq 0 ]; then
+	        echo "file succesfully downloaded"
+	else
+		echo "wget failed"
+		        exit
+fi
+
+#runs the find command against every pathway designated in the pathways.sh 
+file=/home/jack/documents/scripts/cleanup_paths
+lines=$(cat $file | sed 's/\r//')
 for line in $lines
 do 
-	find -mtime +2 -type f
+	sudo echo $line
+        sudo find $line -mtime -2 -type f -name "*.logs" | xargs ls -al 
 done
+fi
+
+if [ $? -eq 0 ]; then
+	        echo "find succesfully ran"
+	else
+		echo "find failed"
+		        exit
+fi
