cb61d33413b40d25127e78384e1a2539d9a16ac6
root <root@DESKTOP-70QGD1I.localdomain>
2022-07-20 16:52:56 +0100

    protoype reading for testing, deleting done via xargs rm -f

--- find_crontab.sh
+++ find_crontab.sh
@@ -5,7 +5,9 @@ if [ ! -d /home/jack/documents/scripts/old_cleanup_paths ]; then
 	mkdir -p /home/jack/documents/scripts/old_cleanup_paths;
 else
 echo "folder already exists"
+fi
 
+#checks to see if previous command has ran succesfully
 if [ $? -eq 0 ]; then
 	echo "folder created or already exists"
 else
@@ -13,9 +15,25 @@ else
 	exit
 fi
 
+#if statement to make the output file
+if [ ! -d /home/jack/documents/scripts/find_crontab/output ]; then
+	        # make directory if it does not exist
+		       touch /home/jack/documents/scripts/find_crontab/output;
+		else
+			echo "file already exists"
+fi
+#checks to see if previous command has ran succesfully
+if [ $? -eq 0 ]; then
+	        echo "file created or already exists"
+else
+                echo "mkdir failed"
+	        exit
+fi
+
 # copies out previous day's pathways file and renames it with the date at the end.
 cp /home/jack/documents/scripts/cleanup_paths /home/jack/documents/scripts/old_cleanup_paths/cleanup_paths_"$(date)"
 
+#checks to see if previous command has ran succesfully
 if [ $? -eq 0 ]; then
 	        echo "copied succesfully"
 	else
@@ -25,8 +43,9 @@ fi
 
 # retrieves new pathways file from url
 # wget -O is used because we only want to download that specific file not the whole html code
-wget -O /home/jack/documents/scripts/cleanup_paths https://raw.githubusercontent.com/jack-percival/Find-crontab/main/cleanup_paths
+wget -O /home/jack/documents/scripts/cleanup_paths https://raw.githubusercontent.com/jack-percival/Find-crontab/master/cleanup_paths 
 
+#checks to see if previous command has ran succesfully
 if [ $? -eq 0 ]; then
 	        echo "file succesfully downloaded"
 	else
@@ -41,14 +60,15 @@ for line in $lines
 do 
 	if [ $line-d ]; then
 			sudo echo $line
-			sudo find $line -mtime +2 -type f -name "*.logs" | tr " " "\n"  >> /home/jack/documents/scripts/find_crontab/output
+			sudo find $line -mtime -2 -type f -name "*.logs" | tr " " "\n"  >> /home/jack/documents/scripts/find_crontab/output
+	    	        sudo find $line -mtime -2 -type f -name "*.logs" | xargs rm -f
 		else
 			echo "directory does not exist"
 	fi
 
 done
-fi
 
+#checks to see if previous command has ran succesfully
 if [ $? -eq 0 ]; then
 	        echo "find succesfully ran"
 	else
@@ -57,9 +77,17 @@ if [ $? -eq 0 ]; then
 
 fi
 
-delete_file=/home/jack/documents/scripts/find_crontab/output
-delete_lines=$(cat $delete_file)
-for line in $delete_lines
-do
-       	sudo echo $line 
-															done
+#delete_file=/home/jack/documents/scripts/find_crontab/output
+#delete_lines=$(cat $delete_file)
+#for line in $delete_lines
+#do
+ #      sudo rm -f 
+#done
+
+#checks to see if previous command has ran succesfully
+if [ $? -eq 0 ]; then
+	                echo "delete succesfully ran"
+			        else
+					                echo "delete failed" 
+						       	exit
+fi
